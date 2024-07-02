aeb01e2c280d59e87b26b405c0b0c5ed05167cd7
Peter Strøiman <peter@stroiman.com>
2024-07-02 10:51:27 +0200

    Sensible tabstops

--- init.lua
+++ init.lua
@@ -5,6 +5,10 @@ end
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
 vim.keymap.set("n", "<leader>ve", ":tabnew $MYVIMRC<cr>", { desc = "Open neovim configuration file" })
 vim.keymap.set("n", "<leader>vs", reload, { desc = "Re-source neovim configuration file" })
 vim.keymap.set("i", "jk", "<esc>", { desc = "Exit insert mode" })
